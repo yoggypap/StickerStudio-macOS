14d98d70e7bce62cf211f1250fac964ad351b5e2
Владимир Александров <yoggypap@MacBook-Pro-Vladimir.local>
2026-09-16 19:44:40 +0300

    Remove obsolete performance test scripts

--- test_perf2.py
+++
@@ -1,9 +0,0 @@
-import time
-from PIL import Image
-from chroma_key import ChromaKey
-
-img = Image.new("RGBA", (640, 480), (0, 255, 0, 255))
-t0 = time.time()
-ChromaKey.prepare_for_vp9(img, 3)
-t1 = time.time()
-print("Time vp9:", t1 - t0)
